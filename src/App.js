4b16206de75f88c8158da59b5f10b6032f61400f
Arjun Samrat <arjunsamrat0705@gmail.com>
2022-09-21 21:53:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import About from "./Components/about/About"
+import Contact from "./Components/contact/Contact"
+import Experience from './Components/experience/Experience';
+import Footer from './Components/footer/Footer';
+import Header from './Components/header/Header';
+import Nav from './Components/nav/Nav';
+import Portfolio from './Components/portfolio/Portfolio';
+import Services from './Components/services/Services';
+
+
+
+function App() {
+  return (
+    <>
+    <About />
+    <Contact />
+    <Experience />
+    <Footer />
+    <Header />
+    <Nav />
+    <Portfolio />
+    <Services/>
+    </>
+  );
+}
+
+export default App;
